e83cc471fbaf942abaa76b5cc748920d6986127b
Nicholas Dowell <lykevv@gmail.com>
2017-05-28 11:25:59 -0700

    I've cleand up
    now

--- src/Tester.java
+++ src/Tester.java
@@ -3,11 +3,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		System.out.println("please add test code");
-		System.out.println("code has been tested");
-		System.out.println("code has been tested");
-		System.out.println("code has been tested");
-		System.out.println("code has been tested");
-		System.out.println("code has been tested");
+		//removed unnecessary lines
 		
 	}
 }
