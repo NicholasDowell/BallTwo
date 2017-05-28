26314477ff6a79e52cff226c53ca1f31a3367ee9
Nicholas Dowell <lykevv@gmail.com>
2017-05-28 11:22:49 -0700

    Spammed thing
    is good

--- src/Tester.java
+++ src/Tester.java
@@ -4,5 +4,10 @@ public class Tester {
 	public static void main(String[] args) {
 		System.out.println("please add test code");
 		System.out.println("code has been tested");
+		System.out.println("code has been tested");
+		System.out.println("code has been tested");
+		System.out.println("code has been tested");
+		System.out.println("code has been tested");
+		
 	}
 }
